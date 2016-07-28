3d7389ba2bf4faddc8fb9075f44d9c53921689b8
Amin Jamali <ajamali@pivotal.io>
2016-07-28 08:12:49 -0400

    Move packer configuration into respective platform
    Signed-off-by: Ben Moss <bmoss@pivotal.io>

---
+++ scripts/create-aws-stemcell.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/env ruby
+
+require 'tmpdir'
+require 'open3'
+require 'securerandom'
+require 'pathname'
+require 'tmpdir'
+require 'fileutils'
+require 'mkmf'
+require_relative '../erb_templates/templates.rb'
+
+BASE_AMI_ID = File.read("windows-ami/version").chomp
+VERSION = File.read("version/number").chomp
+DEPS_URL = File.read("bosh-agent-deps-zip/url").chomp
+AGENT_URL = File.read("bosh-agent-zip/url").chomp
+
+AGENT_COMMIT = `git --git-dir bosh-agent/.git rev-parse HEAD`.chomp
+
+OUTPUT_DIR = ENV.fetch("OUTPUT_DIR")
+AWS_ACCESS_KEY = ENV.fetch("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = ENV.fetch("AWS_SECRET_KEY")
+VPC_ID = ENV.fetch("VPC_ID")
+SUBNET_ID = ENV.fetch("SUBNET_ID")
+AMI_NAME = "BOSH-" + SecureRandom.uuid
+
+def parse_ami_id(line)
+  unless line.include?("amazon-ebs,artifact,0,id,")
+    return
+  end
+  return line.split(",").last.split(":").last
+end
+
+def run_packer(config_path)
+  Dir.chdir(File.dirname(config_path)) do
+    command = %{
+      packer build \
+      -var "aws_access_key=#{AWS_ACCESS_KEY}" \
+      -var "aws_secret_key=#{AWS_SECRET_KEY}" \
+      -var "deps_url=#{DEPS_URL}" \
+      -var "agent_url=#{AGENT_URL}" \
+      -var "base_ami_id=#{BASE_AMI_ID}" \
+      -var "vpc_id=#{VPC_ID}" \
+      -var "subnet_id=#{SUBNET_ID}" \
+      -var "ami_name=#{AMI_NAME}" \
+      #{config_path}
+    }
+
+    ami_id = nil
+    Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+      stdout.each_line do |line|
+        puts line
+        if ami_id == nil
+          ami_id = parse_ami_id(line)
+        end
+      end
+      exit_status = wait_thr.value
+      if exit_status != 0
+        puts stderr.readlines
+        puts "packer build failed #{exit_status}"
+        exit(1)
+      end
+    end
+    ami_id
+  end
+end
+
+def exec_command(cmd)
+  `#{cmd}`
+  exit 1 unless $?.success?
+end
+
+if find_executable('packer') == nil
+  abort("ERROR: cannot find 'packer' on the path")
+end
+
+FileUtils.mkdir_p(OUTPUT_DIR)
+output_dir = File.absolute_path(OUTPUT_DIR)
+
+BUILDER_PATH = File.expand_path("../..", __FILE__)
+packer_config = File.join(BUILDER_PATH, "aws","packer.json")
+ami_id = run_packer(packer_config)
+
+Dir.mktmpdir do |dir|
+  MFTemplate.new("#{BUILDER_PATH}/erb_templates/aws/stemcell.MF.erb", VERSION, ami_id: ami_id).save(dir)
+  ApplySpecTemplate.new("#{BUILDER_PATH}/erb_templates/apply_spec.yml.erb", AGENT_COMMIT).save(dir)
+  exec_command("touch #{dir}/image")
+
+  stemcell_filename = "light-bosh-stemcell-#{VERSION}-aws-xen-hvm-windows2012R2-go_agent.tgz"
+
+  exec_command("tar czvf #{File.join(output_dir, stemcell_filename)} -C #{dir} stemcell.MF apply_spec.yml image")
+end
---
+++ scripts/create-vsphere-stemcell.rb
@@ -0,0 +1,141 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'zlib'
+require 'tmpdir'
+require 'open3'
+require 'securerandom'
+require 'pathname'
+require 'mkmf'
+require_relative '../erb_templates/templates.rb'
+
+# concourse inputs
+VERSION = File.read("version/number").chomp
+DEPS_URL = File.read("bosh-agent-deps-zip/url").chomp
+AGENT_URL = File.read("bosh-agent-zip/url").chomp
+AGENT_COMMIT = `git --git-dir bosh-agent/.git rev-parse HEAD`.chomp
+
+OUTPUT_DIR = ENV.fetch("OUTPUT_DIR")
+ISO_URL = ENV.fetch('ISO_URL')
+ISO_CHECKSUM_TYPE = ENV.fetch('ISO_CHECKSUM_TYPE')
+ISO_CHECKSUM = ENV.fetch('ISO_CHECKSUM')
+MEMSIZE = ENV.fetch('MEMSIZE')
+NUMVCPUS = ENV.fetch('NUMVCPUS')
+REMOTE_HOST = ENV.fetch('REMOTE_HOST')
+REMOTE_PORT = ENV.fetch('REMOTE_PORT')
+REMOTE_DATASTORE = ENV.fetch('REMOTE_DATASTORE')
+REMOTE_CACHE_DATASTORE = ENV.fetch('REMOTE_CACHE_DATASTORE')
+REMOTE_CACHE_DIRECTORY = ENV.fetch('REMOTE_CACHE_DIRECTORY')
+REMOTE_USERNAME = ENV.fetch('REMOTE_USERNAME')
+REMOTE_PASSWORD = ENV.fetch('REMOTE_PASSWORD')
+ADMINISTRATOR_PASSWORD = ENV.fetch('ADMINISTRATOR_PASSWORD')
+
+# erb_templates/network-interface-settings.xml
+GUEST_NETWORK_ADDRESS = ENV.fetch('GUEST_NETWORK_ADDRESS')
+GUEST_NETWORK_MASK = ENV.fetch('GUEST_NETWORK_MASK')
+GUEST_NETWORK_GATEWAY = ENV.fetch('GUEST_NETWORK_GATEWAY')
+
+def gzip_file(name, output)
+  Zlib::GzipWriter.open(output) do |gz|
+   File.open(name) do |fp|
+     while chunk = fp.read(32 * 1024) do
+       gz.write chunk
+     end
+   end
+   gz.close
+  end
+end
+
+def packer_command(command, config_path)
+  Dir.chdir(File.dirname(config_path)) do
+    args = %{
+      packer #{command} \
+      -var "iso_url=#{ISO_URL}" \
+      -var "iso_checksum_type=#{ISO_CHECKSUM_TYPE}" \
+      -var "iso_checksum=#{ISO_CHECKSUM}" \
+      -var "deps_url=#{DEPS_URL}" \
+      -var "agent_url=#{AGENT_URL}" \
+      -var "memsize=#{MEMSIZE}" \
+      -var "numvcpus=#{NUMVCPUS}" \
+      -var "remote_host=#{REMOTE_HOST}" \
+      -var "remote_port=#{REMOTE_PORT}" \
+      -var "remote_datastore=#{REMOTE_DATASTORE}" \
+      -var "remote_cache_datastore=#{REMOTE_CACHE_DATASTORE}" \
+      -var "remote_cache_directory=#{REMOTE_CACHE_DIRECTORY}" \
+      -var "remote_username=#{REMOTE_USERNAME}" \
+      -var "remote_password=#{REMOTE_PASSWORD}" \
+      -var "administrator_password=#{ADMINISTRATOR_PASSWORD}" \
+      -var "winrm_host=#{GUEST_NETWORK_ADDRESS}" \
+      #{config_path}
+    }
+    Open3.popen3(args) do |stdin, stdout, stderr, wait_thr|
+      stdout.each_line do |line|
+        puts line
+      end
+      exit_status = wait_thr.value
+      if exit_status != 0
+        puts stderr.readlines
+        puts "packer failed #{exit_status}"
+        exit(1)
+      end
+    end
+  end
+end
+
+def exec_command(cmd)
+  `#{cmd}`
+  exit 1 unless $?.success?
+end
+
+def install_ovftool
+  files = Dir.glob("**/VMware-ovftool-*.bundle")
+  if files == []
+    abort("ERROR: cannot find 'ovftool' bundle")
+  end
+  ovftoolBundle = files[0]
+  File.chmod(0777, ovftoolBundle)
+  exec_command("#{ovftoolBundle} --required --eulas-agreed")
+end
+
+install_ovftool
+
+if find_executable('ovftool') == nil
+  abort("ERROR: cannot find 'ovftool' on the path")
+end
+
+if find_executable('packer') == nil
+  abort("ERROR: cannot find 'packer' on the path")
+end
+
+FileUtils.mkdir_p(OUTPUT_DIR)
+output_dir = File.absolute_path(OUTPUT_DIR)
+
+IMAGE_PATH = "#{output_dir}/image"
+
+BUILDER_PATH=File.expand_path("../..", __FILE__)
+
+NetworkInterfaceSettingsTemplate.new(
+  "#{BUILDER_PATH}/erb_templates/vsphere/network-interface-settings.xml.erb",
+  GUEST_NETWORK_ADDRESS,GUEST_NETWORK_MASK,GUEST_NETWORK_GATEWAY).save("#{BUILDER_PATH}/vsphere")
+
+packer_config = File.join(BUILDER_PATH, "vsphere", "packer.json")
+packer_command('validate', packer_config)
+packer_command('build', packer_config)
+
+ova_file = Dir.glob('**/packer-vmware-iso.ova' ).select { |fn| File.file?(fn) }
+if ova_file.length == 0
+  abort("ERROR: unable to find packer-vmware-iso.ova")
+end
+gzip_file(ova_file[0], "#{IMAGE_PATH}")
+
+IMAGE_SHA1=`sha1sum #{IMAGE_PATH} | cut -d ' ' -f 1 | xargs echo -n`
+
+Dir.mktmpdir do |dir|
+  MFTemplate.new("#{BUILDER_PATH}/erb_templates/vsphere/stemcell.MF.erb", VERSION, sha1: IMAGE_SHA1).save(dir)
+  ApplySpecTemplate.new("#{BUILDER_PATH}/erb_templates/apply_spec.yml.erb", AGENT_COMMIT).save(dir)
+  FileUtils.cp("#{IMAGE_PATH}", dir)
+
+  stemcell_filename = "bosh-stemcell-#{VERSION}-vsphere-esxi-windows2012R2-go_agent.tgz"
+
+  exec_command("tar czvf #{File.join(output_dir, stemcell_filename)} -C #{dir} stemcell.MF apply_spec.yml image")
+end
