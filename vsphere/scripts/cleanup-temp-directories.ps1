deb704094e52f042b381430c1ed5bb695634221a
Sunjay Bhatia <sbhatia@pivotal.io>
2016-08-08 12:35:23 -0400

    Don't remove VMWare Guest Tools logs
    [#127302591]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- vsphere/scripts/cleanup-temp-directories.ps1
+++ vsphere/scripts/cleanup-temp-directories.ps1
@@ -1,1 +1,4 @@
-Remove-Item -Force -Recurse -Path C:\Windows\Temp\*
+Get-ChildItem -Path C:\Windows\Temp |
+    Select-Object -expandproperty fullname |
+    Where { $_ -notlike "C:\Windows\Temp\vm*" } |
+    Remove-Item -Force -Recurse
