044386d1b978a5ccdfe8b8924fd33b0f54e27442
Amin Jamali <ajamali@pivotal.io>
2016-08-12 11:48:48 -0400

    Add windows-updates output to the errand
    [#127651779]
    
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- bosh-windows-acceptance-tests/assets/errand-release/jobs/errand/templates/run.ps1
+++ bosh-windows-acceptance-tests/assets/errand-release/jobs/errand/templates/run.ps1
@@ -1,2 +1,8 @@
 Write-Host "I am executing a simple bosh errand"
 Get-ChildItem
+
+$logPath = "/var/vcap/sys/log/errand"
+New-Item -ItemType Directory -Force -Path  $logPath
+
+$unixTimestamp = ([int][double]::Parse((Get-Date -UFormat %s)))
+wmic qfe list brief /format:texttablewsys > "$logPath/update-list-$unixTimestamp.txt"
--- bosh-windows-acceptance-tests/main_test.go
+++ bosh-windows-acceptance-tests/main_test.go
@@ -210,7 +210,7 @@ var _ = Describe("BOSH Windows", func() {
 		err = bosh.Run(fmt.Sprintf("-d %s deploy", manifestPath))
 		Expect(err).To(BeNil())
 
-		err = bosh.Run(fmt.Sprintf("-d %s run errand errand", manifestPath))
+		err = bosh.Run(fmt.Sprintf("-d %s run errand errand --download-logs --logs-dir updates-list", manifestPath))
 		Expect(err).To(BeNil())
 	})
 })
