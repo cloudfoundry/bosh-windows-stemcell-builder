4755cd1fd8a8c674ceaf1ea123b6d7cc64ec6874
Sunjay Bhatia <sbhatia@pivotal.io>
2016-08-15 13:55:50 -0400

    Export updates list to .csv
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- bosh-windows-acceptance-tests/assets/errand-release/jobs/errand/templates/run.ps1
+++ bosh-windows-acceptance-tests/assets/errand-release/jobs/errand/templates/run.ps1
@@ -5,4 +5,4 @@ $logPath = "/var/vcap/sys/log/errand"
 New-Item -ItemType Directory -Force -Path  $logPath
 
 $unixTimestamp = ([int][double]::Parse((Get-Date -UFormat %s)))
-wmic qfe list brief /format:texttablewsys > "$logPath/update-list-$unixTimestamp.txt"
+wmic qfe list brief /format:csv | ? {$_.trim() -ne "" } | > "$logPath/update-list-$unixTimestamp.csv"
