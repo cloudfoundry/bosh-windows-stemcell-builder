0634ed2e4445aeb64f11449d49f9f37c3a53e960
Pivotal <cf-netgarden-eng@pivotal.io>
2016-08-24 16:39:35 -0400

    Revert "Revert "Add WindowsUpdate to vsphere image""
    This adds back the use of PSWindowsUpdate to handle updates.
    
    [#126614677]
    
    This reverts commit ba0d2d59cb195a091c1343f6da2490579aedde93.

--- vsphere/scripts/microsoft-updates.bat
+++ vsphere/scripts/microsoft-updates.bat
@@ -8,3 +8,5 @@ echo Set ServiceManager = CreateObject("Microsoft.Update.ServiceManager") > a:\t
 echo Set NewUpdateService = ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"") >> a:\temp.vbs
 
 cscript a:\temp.vbs
+
+net start wuauserv
