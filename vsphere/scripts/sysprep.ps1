15508c1405f26ea2b1a9b438d162742fca74d26e
Sunjay Bhatia <sbhatia@pivotal.io>
2016-09-09 13:44:34 -0400

    Turn off Automatic Updates on vsphere stemcells
    - Temproray turn off windows updates for faster feedback
    
    [#129885845]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- vsphere/scripts/sysprep.ps1
+++ vsphere/scripts/sysprep.ps1
@@ -39,6 +39,28 @@ $PostUnattend = @"
             <UserLocale>en-US</UserLocale>
         </component>
         <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <FirstLogonCommands>
+                <SynchronousCommand wcm:action="add">
+                    <CommandLine>cmd.exe /c net stop wuauserv</CommandLine>
+                    <Order>1</Order>
+                    <Description>Stop Windows Update Service</Description>
+                </SynchronousCommand>
+                <SynchronousCommand wcm:action="add">
+                    <CommandLine>cmd.exe /c reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f</CommandLine>
+                    <Description>Disable AUOptions Registry Key</Description>
+                    <Order>2</Order>
+                </SynchronousCommand>
+                <SynchronousCommand wcm:action="add">
+                    <CommandLine>cmd.exe /c reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v EnableFeaturedSoftware /t REG_DWORD /d 0 /f</CommandLine>
+                    <Description>Disable EnableFeaturedSoftware Registry Key</Description>
+                    <Order>3</Order>
+                </SynchronousCommand>
+                <SynchronousCommand wcm:action="add">
+                    <CommandLine>cmd.exe /c reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v IncludeRecommendedUpdates /t REG_DWORD /d 0 /f</CommandLine>
+                    <Description>Disable IncludeRecommendedUpdates Registry Key</Description>
+                    <Order>4</Order>
+                </SynchronousCommand>
+            </FirstLogonCommands>
             <OOBE>
                 <HideEULAPage>true</HideEULAPage>
                 <ProtectYourPC>1</ProtectYourPC>
