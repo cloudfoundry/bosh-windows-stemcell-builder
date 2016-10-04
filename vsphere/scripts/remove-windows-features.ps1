95ae841caf493e682eea51167be35fbf2a564487
Amin Jamali <ajamali@pivotal.io>
2016-10-04 10:41:00 -0400

    Revert the reverts on master + fixes on top of dev
    - Cleanup WinSxS folder and compact disk
    
    - Upload ultradefrag.zip instead of placing in the floppy drive
      Packer hard codes the max size of the inserted floppy drive to 1.44MB
    - Fix invalid JSON
    - Move ultradefrag-zip before validating packer file
    
    [#126039603]
    
    Signed-off-by: Charlie Vieth <cviethjr@pivotal.io>

--- vsphere/scripts/remove-windows-features.ps1
+++ vsphere/scripts/remove-windows-features.ps1
@@ -4,3 +4,7 @@ Remove-WindowsFeature -Name 'Powershell-ISE'
 Get-WindowsFeature |
 ? { $_.InstallState -eq 'Available' } |
 Uninstall-WindowsFeature -Remove
+
+# Cleanup WinSxS folder: https://technet.microsoft.com/en-us/library/dn251565.aspx
+Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
+Dism.exe /online /Cleanup-Image /SPSuperseded
