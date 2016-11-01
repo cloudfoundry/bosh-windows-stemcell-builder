3137fbb058cc210633aba56d4c238d6d44898709
Dave Goddard <dave@goddard.id.au>
2016-11-01 12:08:25 -0400

    Improve error handling for LGPO
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- vsphere/scripts/run-policies.ps1
+++ vsphere/scripts/run-policies.ps1
@@ -1,13 +1,40 @@
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-  function Unzip
-  {
-      param([string]$zipfile, [string]$outpath)
-
-      [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-
-      rm $zipfile
+try {
+  if(!(Test-Path -Path "C:\bosh" )){
+    mkdir "C:\bosh"
+  }
+  if(!(Test-Path -Path "C:\var\vcap\bosh\bin" )){
+    mkdir "C:\var\vcap\bosh\bin"
   }
 
-Invoke-WebRequest "https://msdnshared.blob.core.windows.net/media/2016/09/LGPOv2-PRE-RELEASE.zip" -OutFile "C:\bosh\lgpov2.zip"
-Unzip "C:\bosh\lgpov2.zip" "C:\var\vcap\bosh\bin\"
-C:\var\vcap\bosh\bin\LGPO.exe /g A:\policy-baselines
+  Add-Type -AssemblyName System.IO.Compression.FileSystem
+  function Unzip
+  {
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+
+    rm $zipfile
+  }
+
+  Invoke-WebRequest "https://msdnshared.blob.core.windows.net/media/2016/09/LGPOv2-PRE-RELEASE.zip" -OutFile "C:\bosh\lgpov2.zip"
+  if (-Not (Test-Path "C:\bosh\lgpov2.zip")) {
+    Write-Error "ERROR: could not download LGPO"
+    Exit 1
+  }
+
+  Unzip "C:\bosh\lgpov2.zip" "C:\var\vcap\bosh\bin\"
+  if (-Not (Test-Path "C:\var\vcap\bosh\bin\LGPO.exe")) {
+    Write-Error "ERROR: could not extract LGPO"
+    Exit 1
+  }
+
+  C:\var\vcap\bosh\bin\LGPO.exe /g A:\policy-baselines
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "Error: LGPO exited with code ${LASTEXITCODE}"
+    Exit $LASTEXITCODE
+  }
+} catch {
+  Write-Error $_.Exception.Message
+  Exit 1
+}
+Exit 0
