2314dd7078ab0e4cc86b8ac4894c3ff098382788
Dave Goddard <dave@goddard.id.au>
2016-12-16 16:39:31 -0500

    Disable winrm with passion
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- scripts/disable-winrm.ps1
+++ scripts/disable-winrm.ps1
@@ -1,2 +1,14 @@
 # Disable winrm
-Get-Service WinRM | Stop-Service -PassThru | Set-Service -StartupType disabled
+Get-Service WinRM | Stop-Service -PassThru | Set-Service -StartupType Disabled
+Set-Service -name WinRM -StartupType Disabled
+
+get-wmiobject win32_service | where Name -eq WinRM
+
+winrm set winrm/config/service '@{AllowUnencrypted="false"}'
+winrm set winrm/config/service/auth '@{Basic="false"}'
+
+netsh advfirewall firewall set rule name="WinRM 5985" new enable=no
+netsh advfirewall firewall set rule name="WinRM 5986" new enable=no
+
+net stop winrm
+sc config winrm start=disabled
