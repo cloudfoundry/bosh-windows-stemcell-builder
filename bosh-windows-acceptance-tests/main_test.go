908761d4f7f25c9804834cea8ea0bda0dde1d0c9
Amin Jamali <ajamali@pivotal.io>
2016-12-28 19:34:45 -0500

    Show logs when failing errand

--- bosh-windows-acceptance-tests/main_test.go
+++ bosh-windows-acceptance-tests/main_test.go
@@ -368,7 +368,7 @@ var _ = Describe("BOSH Windows", func() {
 	})
 
 	It("checks system dependencies and security", func() {
-		err := bosh.Run(fmt.Sprintf("-d %s run-errand check-system", deploymentName))
+		err := bosh.Run(fmt.Sprintf("-d %s run-errand --download-logs check-system", deploymentName))
 		Expect(err).To(Succeed())
 	})
 
