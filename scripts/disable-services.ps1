d9be09ef32629d0f1dbb004199ee08bc7e1fc127
Natalie Arellano <narellano@pivotal.io>
2017-01-19 11:32:38 -0500

    Disable IIS Web Server
    [#136455785]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- scripts/disable-services.ps1
+++ scripts/disable-services.ps1
@@ -2,5 +2,6 @@ $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
 Get-Service WinRM  | Set-Service -StartupType Disabled
+Get-Service W3Svc  | Set-Service -StartupType Disabled
 
 Exit 0
