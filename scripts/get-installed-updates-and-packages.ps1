6b45d5a7ad7e9a8ed09f5f6ba03c4650f422ef5a
Amin Jamali <ajamali@pivotal.io>
2017-03-03 14:11:50 -0500

    Remove extra backslash in ps1 script
    Signed-off-by: Charlie Vieth <cviethjr@pivotal.io>
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- scripts/get-installed-updates-and-packages.ps1
+++ scripts/get-installed-updates-and-packages.ps1
@@ -5,8 +5,8 @@
 #>
 
 param(
-    [String]$UpdatesPath="installed-updates.json",
-    [String]$FeaturesPath="installed-features.json"
+    [String]$UpdatesPath="C:\\bosh\\installed-updates.json",
+    [String]$FeaturesPath="C:\\bosh\\installed-features.json"
 )
 
 $ErrorActionPreference = "Stop";
