974f69a0bce9a9acb533aa57bc4d11b2f766f730
David Jahn <djahn@pivotal.io>
2017-03-07 11:29:38 -0500

    remove unused resource dirs from update-vmx job
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- scripts/create-vmx-add-updates.rb
+++ scripts/create-vmx-add-updates.rb
@@ -17,8 +17,6 @@ end
 
 FileUtils.mkdir_p(File.join("stemcell-builder","build"))
 FileUtils.cp_r("windows-stemcell-dependencies",File.join("stemcell-builder","build","windows-stemcell-dependencies"))
-FileUtils.cp_r("compiled-agent",File.join("stemcell-builder","build","compiled-agent"))
-FileUtils.cp_r("version",File.join("stemcell-builder","build","version"))
 FileUtils.cp_r("vmx-version",File.join("stemcell-builder","build","vmx-version"))
 
 Dir.chdir "stemcell-builder" do
