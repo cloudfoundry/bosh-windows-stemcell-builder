8dee1a0941f36d5d6148fd5897a8e97c0fca4bf2
David Jahn <djahn@pivotal.io>
2017-03-08 14:09:26 -0500

    make gcp start script use new stemcell builder class
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

--- scripts/create-gcp-stemcell.rb
+++ scripts/create-gcp-stemcell.rb
@@ -1,92 +1,15 @@
 #!/usr/bin/env ruby
 
-require 'tmpdir'
-require 'open3'
-require 'securerandom'
-require 'pathname'
-require 'tmpdir'
+require_relative 'exec-command'
 require 'fileutils'
-require 'mkmf'
-require 'json'
-require 'yaml'
-require 'tempfile'
-require_relative '../erb_templates/templates.rb'
 
-VERSION = File.read("version/number").chomp
+FileUtils.mkdir_p(File.join("stemcell-builder","build"))
+FileUtils.cp_r("windows-stemcell-dependencies", File.join("stemcell-builder","build","windows-stemcell-dependencies"))
+FileUtils.cp_r("compiled-agent", File.join("stemcell-builder","build","compiled-agent"))
+FileUtils.cp_r("base-gcp-image", File.join("stemcell-builder","build","base-gcp-image"))
+FileUtils.cp_r("version", File.join("stemcell-builder","build","version"))
 
-AGENT_COMMIT = File.read("compiled-agent/sha").chomp
-
-BASE_IMAGE = JSON.parse(File.read(Dir.glob("base-gcp-image/base-gcp-image-*.json")[0]).chomp)["base_image"]
-if BASE_IMAGE.empty?
-  abort("ERROR: cannot find 'base_image' in GCP image json")
-end
-
-OUTPUT_DIR = ENV.fetch("OUTPUT_DIR")
-ACCOUNT_JSON = ENV.fetch('ACCOUNT_JSON')
-PROJECT_ID = JSON.parse(ACCOUNT_JSON)['project_id']
-OS_VERSION = ENV.fetch("OS_VERSION")
-
-def run_packer(config_path)
-  Dir.chdir(File.dirname(config_path)) do
-    command = %{
-      packer build \
-      -machine-readable \
-      -debug \
-      #{config_path}
-    }
-
-    image = nil
-    Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
-      stdout.each_line do |line|
-        puts line
-        if line.include?(",artifact,0,id,")
-          image = line.split(",").last.chomp
-        end
-      end
-      exit_status = wait_thr.value
-      if exit_status != 0
-        puts stderr.readlines
-        puts "packer build failed #{exit_status}"
-        exit(1)
-      end
-    end
-    image
-  end
-end
-
-def exec_command(cmd)
-  output=`#{cmd}`.chomp
-  exit 1 unless $?.success?
-  output
-end
-
-if find_executable('packer').nil?
-  abort("ERROR: cannot find 'packer' on the path")
-end
-
-FileUtils.mkdir_p(OUTPUT_DIR)
-output_dir = File.absolute_path(OUTPUT_DIR)
-
-BUILDER_PATH = File.expand_path("../..", __FILE__)
-account_json = Tempfile.new(['account','.json']).tap(&:close).path
-File.write(account_json, ACCOUNT_JSON)
-gcp_config = File.join(BUILDER_PATH, "gcp")
-
-GCPPackerJsonTemplate.new("#{BUILDER_PATH}/erb_templates/gcp/packer.json.erb",
-                          account_json, PROJECT_ID, BASE_IMAGE).save(gcp_config)
-
-image_name = run_packer(File.join(gcp_config, "packer.json"))
-if image_name.nil? || image_name.empty?
-  abort("ERROR: could not parse GCP Image Name")
-end
-image_self_link = "https://www.googleapis.com/compute/v1/projects/#{PROJECT_ID}/global/images/#{image_name}"
-
-Dir.mktmpdir do |dir|
-  MFTemplate.new("#{BUILDER_PATH}/erb_templates/gcp/stemcell.MF.erb", VERSION, image_self_link: image_self_link, os_version: OS_VERSION).save(dir)
-  ApplySpecTemplate.new("#{BUILDER_PATH}/erb_templates/apply_spec.yml.erb", AGENT_COMMIT).save(dir)
-  exec_command("touch #{dir}/image")
-
-  stemcell_filename = "light-bosh-stemcell-#{VERSION}-google-kvm-windows2012R2-go_agent.tgz"
-
-  exec_command("tar czvf #{File.join(output_dir, stemcell_filename)} -C #{dir} stemcell.MF apply_spec.yml image")
+Dir.chdir "stemcell-builder" do
+  exec_command("bundle install")
+  exec_command("rake build:gcp")
 end
---
+++ scripts/exec-command.rb
@@ -0,0 +1,14 @@
+require 'open3'
+
+def exec_command(cmd)
+  STDOUT.sync = true
+  Open3.popen2(cmd) do |stdin, out, wait_thr|
+    out.each_line do |line|
+      puts line
+    end
+    exit_status = wait_thr.value
+    if exit_status != 0
+      raise "error running command: #{cmd}"
+    end
+  end
+end
