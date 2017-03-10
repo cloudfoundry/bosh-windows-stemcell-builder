dc6411ba6fa3d4a481b7b4dc2e6506785f950495
Natalie Arellano <narellano@pivotal.io>
2017-03-10 15:33:34 -0500

    use exec-command.rb for vmx add update script
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- scripts/create-vmx-add-updates.rb
+++ scripts/create-vmx-add-updates.rb
@@ -1,19 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'open3'
+require_relative 'exec-command'
 require 'fileutils'
-
-def exec_command(cmd)
-  Open3.popen2(cmd) do |stdin, out, wait_thr|
-    out.each_line do |line|
-      puts line
-    end
-    exit_status = wait_thr.value
-    if exit_status != 0
-      raise "error running command: #{cmd}"
-    end
-  end
-end
 
 FileUtils.mkdir_p(File.join("stemcell-builder","build"))
 FileUtils.cp_r("windows-stemcell-dependencies",File.join("stemcell-builder","build","windows-stemcell-dependencies"))
