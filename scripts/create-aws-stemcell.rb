5624d73f04df7c60aca2b5f2a10003e5af8fa646
Amin Jamali <ajamali@pivotal.io>
2017-03-15 18:59:20 -0400

    Add bosh-psmodules to ci scripts

--- scripts/create-aws-stemcell.rb
+++ scripts/create-aws-stemcell.rb
@@ -1,12 +1,18 @@
 #!/usr/bin/env ruby
 
-require_relative '../lib/exec_command'
 require 'fileutils'
+
+require_relative '../lib/exec_command'
+require_relative '../lib/zip_file'
 
 FileUtils.mkdir_p(File.join("stemcell-builder","build"))
 FileUtils.cp_r("windows-stemcell-dependencies", File.join("stemcell-builder","build","windows-stemcell-dependencies"))
 FileUtils.cp_r("version", File.join("stemcell-builder","build","version"))
 FileUtils.cp_r("base-amis", File.join("stemcell-builder","build","base-amis"))
+
+directory = File.join(__dir__,"..","bosh-psmodules","modules")
+output = File.join("stemcell-builder","build","bosh-psmodules.zip")
+ZipFile::Generator.new(directory, output).write()
 
 Dir.chdir "stemcell-builder" do
   exec_command("bundle install")
--- scripts/create-vsphere-stemcell.rb
+++ scripts/create-vsphere-stemcell.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby
 
-require 'open3'
-require_relative '../lib/exec_command'
 require 'fileutils'
+
+require_relative '../lib/exec_command'
+require_relative '../lib/zip_file'
 
 FileUtils.mkdir_p(File.join("stemcell-builder","build"))
 FileUtils.cp_r("windows-stemcell-dependencies",File.join("stemcell-builder","build","windows-stemcell-dependencies"))
 FileUtils.cp_r("version",File.join("stemcell-builder","build","version"))
 FileUtils.cp_r("vmx-version",File.join("stemcell-builder","build","vmx-version"))
+
+directory = File.join(__dir__,"..","bosh-psmodules","modules")
+output = File.join("stemcell-builder","build","bosh-psmodules.zip")
+ZipFile::Generator.new(directory, output).write()
 
 Dir.chdir "stemcell-builder" do
   exec_command("bundle install")
