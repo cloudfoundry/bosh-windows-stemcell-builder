745140e8b2b267113fd527e019399e2e7d6073f7
Sunjay Bhatia <sbhatia@pivotal.io>
2017-04-03 13:14:55 -0400

    sysprep create unattend cmdlet/module/tests
    [#142852453]
    
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- bosh-psmodules/modules/BOSH.Sysprep/BOSH.Sysprep.Tests.ps1
+++ bosh-psmodules/modules/BOSH.Sysprep/BOSH.Sysprep.Tests.ps1
@@ -34,5 +34,82 @@ Describe "Enable-LocalSecurityPolicy" {
     }
 }
 
+Describe "Create-Unattend" {
+    BeforeEach {
+        $UnattendDestination=(New-TempDir)
+        $NewPassword="NewPassword"
+        $ProductKey= "ProductKey"
+        $Organization="Organization"
+        $Owner="Owner"
+        {
+            Create-Unattend -UnattendDestination $UnattendDestination `
+                -NewPassword $NewPassword `
+                -ProductKey $ProductKey `
+                -Organization $Organization `
+                -Owner $Owner
+        } | Should Not Throw
+    }
+
+    AfterEach {
+        Remove-Item -Recurse -Force $UnattendDestination
+    }
+
+    It "places the generated Unattend file in the specified directory" {
+        Test-Path (Join-Path $UnattendDestination "unattend.xml") | Should Be $True
+    }
+
+    Context "failure scenarios" {
+        It "throws when there is no new password provided" {
+            {
+                Create-Unattend -UnattendDestination $UnattendDestination `
+                    -ProductKey $ProductKey `
+                    -Organization $Organization `
+                    -Owner $Owner
+            } | Should Throw "Provide an Administrator Password"
+        }
+
+        It "throws when Organization or Owner are not present and Product Key is provided" {
+            {
+                Create-Unattend -UnattendDestination $UnattendDestination `
+                    -NewPassword $NewPassword `
+                    -ProductKey $ProductKey
+            } | Should Throw "Provide an Organization and Owner"
+        }
+
+    }
+
+    Context "the generated Unattend file" {
+        BeforeEach {
+            $unattendPath = (Join-Path $UnattendDestination "unattend.xml")
+            [xml]$unattendXML = Get-Content -Path $unattendPath
+            $ns = New-Object System.Xml.XmlNamespaceManager($unattendXML.NameTable)
+            $ns.AddNamespace("ns", $unattendXML.DocumentElement.NamespaceURI)
+        }
+
+        It "contains a New Password" {
+            $unattendXML.unattend.settings.component.UserAccounts.AdministratorPassword.Value | Should Be $NewPassword
+        }
+
+        It "contains a Product Key, Organization, and Owner when Product Key is provided" {
+            $unattendXML.SelectSingleNode("//ns:ProductKey", $ns).'#text' | Should Be $ProductKey
+            $unattendXML.SelectSingleNode("//ns:RegisteredOrganization", $ns).'#text' | Should Be $Organization
+            $unattendXML.SelectSingleNode("//ns:RegisteredOwner", $ns).'#text' | Should Be $Owner
+        }
+
+        It "when Product Key is not provided, there is no Product Key, Organization, or Owner" {
+            {
+                Create-Unattend -UnattendDestination $UnattendDestination `
+                    -NewPassword $NewPassword
+            } | Should Not Throw
+            [xml]$unattendXML = Get-Content -Path $unattendPath
+            $ns = New-Object System.Xml.XmlNamespaceManager($unattendXML.NameTable)
+            $ns.AddNamespace("ns", $unattendXML.DocumentElement.NamespaceURI)
+            $unattendXML.SelectSingleNode("//ns:ProductKey", $ns).'#text' | Should Be $Null
+            $unattendXML.SelectSingleNode("//ns:RegisteredOrganization", $ns).'#text' | Should Be $Null
+            $unattendXML.SelectSingleNode("//ns:RegisteredOwner", $ns).'#text' | Should Be $Null
+        }
+    }
+}
+
 Remove-Module -Name BOSH.Sysprep -ErrorAction Ignore
 Remove-Module -Name BOSH.Utils -ErrorAction Ignore
--- bosh-psmodules/modules/BOSH.Sysprep/BOSH.Sysprep.psm1
+++ bosh-psmodules/modules/BOSH.Sysprep/BOSH.Sysprep.psm1
@@ -1,8 +1,8 @@
 <#
 .Synopsis
-Sysprep Utilities
+    Sysprep Utilities
 .Description
-This cmdlet enables running sysprep on a BOSH deployed VM
+    This cmdlet enables enabling a local security policy for a stemcell
 #>
 function Enable-LocalSecurityPolicy {
     Param (
@@ -24,4 +24,121 @@ function Enable-LocalSecurityPolicy {
 	Throw "lgpo.exe exited with $LASTEXITCODE"
     }
     Write-Log "Ending LocalSecurityPolicy"
+}
+
+<#
+.Synopsis
+    Sysprep Utilities
+.Description
+    This cmdlet creates the Unattend file for sysprep
+#>
+function Create-Unattend {
+    Param (
+      [string]$UnattendDestination = "C:\Windows\Panther\Unattend",
+      [string]$NewPassword = $(Throw "Provide an Administrator Password"),
+      [string]$ProductKey,
+      [string]$Organization,
+      [string]$Owner
+   )
+
+   Write-Log "Starting Create-Unattend"
+
+   New-Item -ItemType directory $UnattendDestination -Force
+   $UnattendPath = Join-Path $UnattendDestination "unattend.xml"
+
+   Write-Log "Writing unattend.xml to $UnattendPath"
+
+   $ProductKeyXML="<RegisteredOwner />"
+   if ($ProductKey -ne "") {
+      if ($Organization -eq "" -or $Owner -eq "") {
+         Throw "Provide an Organization and Owner"
+      }
+      $ProductKeyXML="<ProductKey>$ProductKey</ProductKey>
+      <RegisteredOrganization>$Organization</RegisteredOrganization>
+      <RegisteredOwner>$Owner</RegisteredOwner>"
+   }
+
+    $PostUnattend = @"
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="specialize">
+        <component xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+            <OEMInformation>
+                <HelpCustomized>false</HelpCustomized>
+            </OEMInformation>
+            <ComputerName>*</ComputerName>
+            <TimeZone>UTC</TimeZone>
+            $ProductKeyXML
+        </component>
+        <component xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+            <RunSynchronous>
+                <RunSynchronousCommand wcm:action="add">
+                    <Description>Disable Windows Updates</Description>
+                    <Order>1</Order>
+                    <Path>C:\Windows\System32\cmd.exe /C C:\disable-updates.bat</Path>
+                    <WillReboot>Never</WillReboot>
+                </RunSynchronousCommand>
+                <RunSynchronousCommand wcm:action="add">
+                    <Description>Apply Group Policies</Description>
+                    <Order>2</Order>
+                    <Path>C:\Windows\System32\cmd.exe /C C:\bosh\lgpo\bin\apply-policies.bat</Path>
+                    <WillReboot>Always</WillReboot>
+                </RunSynchronousCommand>
+            </RunSynchronous>
+        </component>
+        <component xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="Microsoft-Windows-ServerManager-SvrMgrNc" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+            <DoNotOpenServerManagerAtLogon>true</DoNotOpenServerManagerAtLogon>
+        </component>
+        <component xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="Microsoft-Windows-OutOfBoxExperience" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+            <DoNotOpenInitialConfigurationTasksAtLogon>true</DoNotOpenInitialConfigurationTasksAtLogon>
+        </component>
+        <component xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="Microsoft-Windows-Security-SPP-UX" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS">
+            <SkipAutoActivation>true</SkipAutoActivation>
+        </component>
+    </settings>
+    <settings pass="generalize">
+        <component name="Microsoft-Windows-Security-SPP" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <SkipRearm>1</SkipRearm>
+        </component>
+        <component name="Microsoft-Windows-PnpSysprep" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <PersistAllDeviceInstalls>false</PersistAllDeviceInstalls>
+            <DoNotCleanUpNonPresentDevices>false</DoNotCleanUpNonPresentDevices>
+        </component>
+    </settings>
+    <settings pass="oobeSystem">
+        <component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <InputLocale>en-US</InputLocale>
+            <SystemLocale>en-US</SystemLocale>
+            <UILanguage>en-US</UILanguage>
+            <UserLocale>en-US</UserLocale>
+        </component>
+        <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <FirstLogonCommands>
+                <SynchronousCommand wcm:action="add">
+                    <CommandLine>C:\Windows\System32\cmd.exe /c C:\disable-updates.bat</CommandLine>
+                    <Order>1</Order>
+                    <Description>Disable Windows Updates</Description>
+                </SynchronousCommand>
+            </FirstLogonCommands>
+            <OOBE>
+                <HideEULAPage>true</HideEULAPage>
+                <ProtectYourPC>3</ProtectYourPC>
+                <NetworkLocation>Home</NetworkLocation>
+                <HideWirelessSetupInOOBE>true</HideWirelessSetupInOOBE>
+            </OOBE>
+            <TimeZone>UTC</TimeZone>
+            <UserAccounts>
+                <AdministratorPassword>
+                    <Value>$NewPassword</Value>
+                    <PlainText>true</PlainText>
+                </AdministratorPassword>
+            </UserAccounts>
+        </component>
+    </settings>
+</unattend>
+"@
+
+   Out-File -FilePath $UnattendPath -InputObject $PostUnattend -Encoding utf8
+
+   Write-Log "Starting Create-Unattend"
 }
