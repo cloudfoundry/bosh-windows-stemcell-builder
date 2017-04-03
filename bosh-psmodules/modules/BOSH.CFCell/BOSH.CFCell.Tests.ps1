2b1039a0a21b3931029f71235f69e4d829c99368
Amin Jamali <ajamali@pivotal.io>
2017-04-02 20:16:36 -0400

    Import missing Utils module for pester

--- bosh-psmodules/modules/BOSH.CFCell/BOSH.CFCell.Tests.ps1
+++ bosh-psmodules/modules/BOSH.CFCell/BOSH.CFCell.Tests.ps1
@@ -1,6 +1,8 @@
 Remove-Module -Name BOSH.CFCell -ErrorAction Ignore
 Import-Module ./BOSH.CFCell.psm1
 
+Remove-Module -Name BOSH.Utils -ErrorAction Ignore
+Import-Module ../BOSH.Utils/BOSH.Utils.psm1
 
 Describe "Protect-CFCell" {
     It "enables the RDP service and firewall rule" {
@@ -8,3 +10,6 @@ Describe "Protect-CFCell" {
        netstat /p tcp /a | findstr 3389 | Should Not BeNullOrEmpty
     }
 }
+
+Remove-Module -Name BOSH.CFCell -ErrorAction Ignore
+Remove-Module -Name BOSH.Utils -ErrorAction Ignore
