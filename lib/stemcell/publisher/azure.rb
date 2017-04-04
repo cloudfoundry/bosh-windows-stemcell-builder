7dcb009124832ce1df69270d361f40d774268abe
Natalie Arellano <narellano@pivotal.io>
2017-04-04 14:02:16 -0400

    Add error checking for invalid api requests
    [#138373423]
    
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

--- lib/stemcell/publisher/azure.rb
+++ lib/stemcell/publisher/azure.rb
@@ -5,10 +5,19 @@ module Stemcell
 	module Publisher
 		class Azure
 			def self.publish(vm_to_add, api_key, url, sku_string)
-				offer_data = obtain_offer_data(url, api_key)
-				update_body = self.json(offer_data, vm_to_add, sku_string)
-				update_offer(url, update_body, api_key)
-				stage_offer(url, api_key)
+				response = obtain_offer_data(url, api_key)
+				if response.code != '200'
+					raise "could not obtain offer data. expected 200 but got '#{response.code}' additional info: #{response.body} "
+				end
+				update_body = self.json(response.body, vm_to_add, sku_string)
+				response = update_offer(url, update_body, api_key)
+				if response.code != '200'
+					raise "could not update offer data. expected 200 but got '#{response.code}' additional info: #{response.body} "
+				end
+				response = stage_offer(url, api_key)
+				if response.code != '200'
+					raise "could not stage offer. expected 200 but got '#{response.code}' additional info: #{response.body} "
+				end
 			end
 
 			def self.json(response_string, vm_to_add, sku_string)
@@ -45,7 +54,8 @@ module Stemcell
 				response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
 					http.request(req)
 				end
-				return response.body
+				puts "response: #{response.body}"
+				return response
 			end
 
 			# Post request to update the offer with latest image
@@ -59,6 +69,7 @@ module Stemcell
 					http.request(req)
 				end
 				puts "response: #{response.body}"
+				return response
 			end
 
 			# Post request to stage the offer
@@ -71,6 +82,7 @@ module Stemcell
 					http.request(req)
 				end
 				puts "response: #{response.body}"
+				return response
 			end
 
 		end
