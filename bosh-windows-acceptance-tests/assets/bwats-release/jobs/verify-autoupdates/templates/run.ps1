0a57c2de264ee202908ae09e82bd2d7831a31416
David Jahn <djahn@pivotal.io>
2017-04-11 11:36:44 -0400

    verify-autoupdates: fallback to CachedAUOptions if AUOptions is not set
    On GCP the AUOptions registry key is not present, fallback to checking
    CachedAUOptions if and only if the AUOptions key is missing.
    
    [#143429647]
    
    Signed-off-by: Charlie Vieth <cviethjr@pivotal.io>

--- bosh-windows-acceptance-tests/assets/bwats-release/jobs/verify-autoupdates/templates/run.ps1
+++ bosh-windows-acceptance-tests/assets/bwats-release/jobs/verify-autoupdates/templates/run.ps1
@@ -10,21 +10,32 @@ $AutoUpdateProperties = Get-ItemProperty -Path $AutoUpdateRegistryPath
 
 if ($AutoUpdateProperties.AUOptions -and
     $AutoUpdateProperties.AUOptions -ne $ExpectedAUOptions) {
-    Write-Error "Error: Expected AUOptions to be '${ExpectedAUOptions}', got '${AutoUpdateProperties.AUOptions}'"
+    Write-Error ("Error: Expected AUOptions to be '{0}', got '{1}'" -f `
+        $ExpectedAUOptions, ($AutoUpdateProperties.AUOptions))
     $ReturnCode = 1
+}
+
+# If the AUOptions key is not present (for example GCP) check CachedAUOptions.
+if ($AutoUpdateProperties.AUOptions -eq $null) {
+    if ($AutoUpdateProperties.CachedAUOptions -ne $null -and
+        $AutoUpdateProperties.CachedAUOptions -ne $ExpectedAUOptions) {
+        Write-Error ("Error: Expected CachedAUOptions to be '{0}', got '{1}'" -f `
+            $ExpectedAUOptions, ($AutoUpdateProperties.CachedAUOptions))
+        $ReturnCode = 1
+    }
 }
 
 if ($AutoUpdateProperties.EnableFeaturedSoftware -and
     $AutoUpdateProperties.EnableFeaturedSoftware -ne $ExpectedEnableFeaturedSoftware) {
-    Write-Error "Error: Expected EnableFeaturedSoftware to be '${ExpectedEnableFeaturedSoftware}'," + `
-                " got '${AutoUpdateProperties.EnableFeaturedSoftware}'"
+    Write-Error ("Error: Expected EnableFeaturedSoftware to be '{0}' got '{1}'" -f `
+        $ExpectedEnableFeaturedSoftware, ($AutoUpdateProperties.EnableFeaturedSoftware))
     $ReturnCode = 1
 }
 
 if ($AutoUpdateProperties.IncludeRecommendedUpdates -and
     $AutoUpdateProperties.IncludeRecommendedUpdates -ne $ExpectedIncludeRecommendedUpdates) {
-    Write-Error "Error: Expected IncludeRecommendedUpdates to be '${ExpectedIncludeRecommendedUpdates}'," + `
-                " got '${AutoUpdateProperties.IncludeRecommendedUpdates}'"
+    Write-Error ("Error: Expected IncludeRecommendedUpdates to be '{0}', got '{1}'" -f `
+        $ExpectedIncludeRecommendedUpdates, ($AutoUpdateProperties.IncludeRecommendedUpdates))
     $ReturnCode = 1
 }
 
