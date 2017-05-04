f74af12a43bb9069ddaaa0362f888767c982e6fe
Sunjay Bhatia <sbhatia@pivotal.io>
2017-05-04 13:21:34 -0400

    all powershell provisioners fail on exception
    [#144795849]
    
    Signed-off-by: Natalie Arellano <narellano@pivotal.io>

--- lib/packer/config/provisioners.rb
+++ lib/packer/config/provisioners.rb
@@ -3,42 +3,55 @@ require 'securerandom'
 module Packer
   module Config
     class Provisioners
+
+      def self.powershell_provisioner(command)
+        {
+          'type' => 'powershell',
+          'inline' => [
+            '$ErrorActionPreference = "Stop";',
+            'trap { $host.SetShouldExit(1) }',
+            command
+          ]
+        }
+      end
+
+      BOSH_PSMODULES = [
+        {
+          'type' => 'file',
+          'source' => 'build/bosh-psmodules.zip',
+          'destination' => 'C:\\provision\\bosh-psmodules.zip'
+        }, {
+          'type' => 'powershell',
+          'scripts' => ['scripts/install-bosh-psmodules.ps1']
+        }
+      ].freeze
+      NEW_PROVISIONER = powershell_provisioner('New-Provisioner')
+      INSTALL_CF_FEATURES = powershell_provisioner('Install-CFFeatures')
+      PROTECT_CF_CELL = powershell_provisioner('Protect-CFCell')
+      OPTIMIZE_DISK = powershell_provisioner('Optimize-Disk')
+      COMPRESS_DISK = powershell_provisioner('Compress-Disk')
+      CLEAR_PROVISIONER = powershell_provisioner('Clear-Provisioner')
+      GET_LOG = powershell_provisioner('Get-Log')
+
       def self.install_windows_updates
         password = SecureRandom.hex(10)+"!"
         return [
-          {
-            'type' => 'powershell',
-            'inline' => ["Add-Account -User Provisioner -Password #{password}"]
-          },
-          {
-            'type' => 'powershell',
-            'inline' => ["Register-WindowsUpdatesTask"]
-          },
+          powershell_provisioner("Add-Account -User Provisioner -Password #{password}"),
+          powershell_provisioner("Register-WindowsUpdatesTask"),
           {
             'type' => 'windows-restart',
             'restart_command' => "powershell.exe -Command Wait-WindowsUpdates -Password #{password} -User Provisioner",
             'restart_timeout' => '12h'
           },
-          {
-            'type' => 'powershell',
-            'inline' => ["Unregister-WindowsUpdatesTask"]
-          },
-          {
-            'type' => 'powershell',
-            'inline' => ["Remove-Account -User Provisioner"]
-          }, {
-            'type' => 'powershell',
-            'inline' => ['Test-InstalledUpdates']
-          }
+          powershell_provisioner("Unregister-WindowsUpdatesTask"),
+          powershell_provisioner("Remove-Account -User Provisioner"),
+          powershell_provisioner("Test-InstalledUpdates")
         ]
       end
 
       def self.download_windows_updates(dest)
         return [
-          {
-            'type' => 'powershell',
-            'inline' => 'List-InstalledUpdates | Out-File -FilePath "C:\\updates.txt" -Encoding ASCII'
-          },
+          powershell_provisioner('List-InstalledUpdates | Out-File -FilePath "C:\\updates.txt" -Encoding ASCII'),
           {
             'type' => 'file',
             'source' => 'C:\\updates.txt',
@@ -55,58 +68,9 @@ module Packer
             'source' => 'build/agent.zip',
             'destination' => 'C:\\provision\\agent.zip'
           },
-          {
-            'type' => 'powershell',
-            'inline' => ["Install-Agent -IaaS #{iaas} -agentZipPath 'C:\\provision\\agent.zip'"]
-          }
+          powershell_provisioner("Install-Agent -IaaS #{iaas} -agentZipPath 'C:\\provision\\agent.zip'")
         ]
       end
-
-      BOSH_PSMODULES = [
-        {
-          'type' => 'file',
-          'source' => 'build/bosh-psmodules.zip',
-          'destination' => 'C:\\provision\\bosh-psmodules.zip'
-        }, {
-          'type' => 'powershell',
-          'scripts' => ['scripts/install-bosh-psmodules.ps1']
-        }
-      ].freeze
-
-      NEW_PROVISIONER = {
-        'type' => 'powershell',
-        'inline' => ['New-Provisioner']
-      }.freeze
-
-      INSTALL_CF_FEATURES = {
-        'type' => 'powershell',
-        'inline' => ['Install-CFFeatures']
-      }.freeze
-
-      PROTECT_CF_CELL = {
-        'type' => 'powershell',
-        'inline' => ['Protect-CFCell']
-      }.freeze
-
-      OPTIMIZE_DISK = {
-        'type' => 'powershell',
-        'inline' => ['Optimize-Disk']
-      }.freeze
-
-      COMPRESS_DISK = {
-        'type' => 'powershell',
-        'inline' => ['Compress-Disk']
-      }.freeze
-
-      CLEAR_PROVISIONER = {
-        'type' => 'powershell',
-        'inline' => ['Clear-Provisioner']
-      }.freeze
-
-      GET_LOG = {
-        'type' => 'powershell',
-        'inline' => ['Get-Log']
-      }.freeze
 
       def self.lgpo_exe
         {
@@ -117,12 +81,7 @@ module Packer
       end
 
       def self.sysprep_shutdown(iaas)
-        return [
-          {
-            'type' => 'powershell',
-            'inline' => ["Invoke-Sysprep -IaaS #{iaas}"]
-          }
-        ]
+        return [powershell_provisioner("Invoke-Sysprep -IaaS #{iaas}")]
       end
     end
   end
