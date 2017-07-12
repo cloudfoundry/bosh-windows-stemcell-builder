801e104b7e7df1f6d6c723118f8c385578b44b15
bot <witchcraft@example.com>
2017-07-12 09:28:58 -0400

    added setup to support new environemnts

--- scripts/setup/run_packer_provisioning.ps1
+++ scripts/setup/run_packer_provisioning.ps1
@@ -1,5 +1,6 @@
 ﻿#This script will allow you to run changes that start from our macOS host to the guest windows OS that will run the packer provisioning steps
 #this allows us to bypass concourse when developing changes to provisioning
+#run setup in install the script
 
 function zipUpPsModules {
     $source = "C:\working_directory\stemcell-builder\bosh-psmodules\modules"
@@ -9,6 +10,24 @@ function zipUpPsModules {
     Add-Type -assembly "system.io.compression.filesystem"
     [io.compression.zipfile]::CreateFromDirectory($Source, $destination)
 }
+
+function setup {
+    #you may get a warning from windows saying that some filenames are too long
+    #this is ok because those files should only be a couple golang test files .
+    New-Item "C:\working_directory" -type directory
+    New-Item "C:\working_directory\version" -type directory
+    New-Item "C:\working_directory\version\number" -type file
+    New-Item "C:\working_directory\stemcell-builder\build" -type directory
+    Copy-Item -Force -Recurse "Z:\workspace\greenhouse-ci\" "C:\working_directory\ci\"
+    Copy-Item -Force -Recurse "Z:\workspace\bosh-windows-stemcell-builder\" "C:\working_directory\stemcell-builder\"
+    Copy-Item -Force -Recurse "Z:\workspace\windows-stemcell-dependencies\" "C:\working_directory\windows-stemcell-dependencies\"
+}
+
+function killallpacker {
+    taskkill /F /IM packer.exe /T
+}
+
+killallpacker
 
 $env:ADMINISTRATOR_PASSWORD="Password123!"
 $env:OS_VERSION="windows2012R2"
@@ -32,7 +51,7 @@ zipUpPsModules
 
 cd "\working_directory"
 
-#.\ci\bosh-windows-stemcell-builder\create-vsphere-vmdk\run.ps1
+.\ci\bosh-windows-stemcell-builder\create-vsphere-vmdk\run.ps1
 
-cd "stemcell-builder"
-packer build -machine-readable -debug -on-error=abort  C:/working_directory/packer.config
+#cd "stemcell-builder"
+#packer build -machine-readable -debug -on-error=abort  C:/working_directory/packer.config
