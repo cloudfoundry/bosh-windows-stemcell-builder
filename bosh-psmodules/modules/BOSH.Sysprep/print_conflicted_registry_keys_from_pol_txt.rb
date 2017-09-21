255cbc9e9f63acc50cc70e8d0b65acefffa03ddf
David Jahn <djahn@pivotal.io>
2017-09-21 10:48:13 -0400

    added merging
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

--- bosh-psmodules/modules/BOSH.Sysprep/print_conflicted_registry_keys_from_pol_txt.rb
+++ bosh-psmodules/modules/BOSH.Sysprep/print_conflicted_registry_keys_from_pol_txt.rb
@@ -1,8 +1,23 @@
-target = 'registry.txt' #filename of the target 
-destination = 'registry-utf8.txt'
-`iconv -f UTF-16LE -t UTF-8 #{target} > #{destination}`
+#!/usr/bin/env ruby
 
-before_uniq = (File.read destination).split("\r\n\r\n").sort
+def convert_to_utf8(name)
+  `iconv -f UTF-16LE -t UTF-8 #{name} > /tmp/registry-tmp`
+  File.read '/tmp/registry-tmp', encoding: 'bom|utf-8'
+end
+
+def merge(a, b)
+  a_no_comment = a.split("\r\n").reject {|x| x[0] == ';' }.join("\r\n").strip
+  b_no_comment = b.split("\r\n").reject {|x| x[0] == ';' }.join("\r\n").strip
+  a_no_comment + "\r\n" + b_no_comment
+end
+
+registry_a_name = 'registry-cis-machine.txt'
+registry_b_name = 'registry-ms-baseline-machine.txt'
+
+registry_a_contents = convert_to_utf8(registry_a_name)
+registry_b_contents = convert_to_utf8(registry_b_name)
+
+before_uniq = merge(registry_a_contents, registry_b_contents).split("\r\n\r\n").sort
 
 puts "before uniq: #{before_uniq.size}"
 after_uniq = before_uniq.uniq
@@ -12,8 +27,8 @@ library = after_uniq.group_by {|x| x.split("\r\n")[1..2].join('\\')}
 dups = library.keys.select {|k| library[k].size > 1}
 
 if(dups.nil?)
-  puts "found 0 conflicts"
+ puts "found 0 conflicts"
 else
-  puts "found #{dups.size} conflicts"
-  dups.each {|x| puts x}
+ puts "found #{dups.size} conflicts"
+ dups.each {|x| puts x}
 end
