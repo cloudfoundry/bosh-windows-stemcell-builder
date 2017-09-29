6d14c8136c8f30ce876a068d81f5c3d34d9e9b67
David Jahn <djahn@pivotal.io>
2017-09-29 11:21:29 -0400

    Install-ContainersFeature is only called on 2016 [#150238350]
    Signed-off-by: Lisa Oakley <loakley@pivotal.io>

--- bosh-psmodules/modules/BOSH.CFCell/BOSH.CFCell.psm1
+++ bosh-psmodules/modules/BOSH.CFCell/BOSH.CFCell.psm1
@@ -118,14 +118,11 @@ function Wait-ForNewIfaces() {
 }
 
 function Install-ContainersFeature {
-  $windowsVersion = (Get-WmiObject -class Win32_OperatingSystem).Caption
-  if ($windowsVersion -Match "2016") {
-    Write-Log "Setting WinRM startup type to automatic"
-    Get-Service | Where-Object {$_.Name -eq "WinRM" } | Set-Service -StartupType Automatic
-    WindowsFeatureInstall("Containers")
-    shutdown /r /c "packer restart" /t 5
-    net stop winrm
-  }
+  Write-Log "Setting WinRM startup type to automatic"
+  Get-Service | Where-Object {$_.Name -eq "WinRM" } | Set-Service -StartupType Automatic
+  WindowsFeatureInstall("Containers")
+  shutdown /r /c "packer restart" /t 5
+  net stop winrm
 }
 
 function Protect-CFCell {
