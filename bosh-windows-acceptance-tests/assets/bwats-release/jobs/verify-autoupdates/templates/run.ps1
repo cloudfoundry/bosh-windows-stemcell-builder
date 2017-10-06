7ca0f180b5ff6ac6b180419d4baf6b43f93d970f
Paul Nikonowicz <pnikonowicz@pivotal.io>
2017-10-06 10:39:58 -0400

    if the windows update service is disabled then assume that windows updates is turned off.
    to test this, we tried to find a way to update windows while the update service is turned off.
    we were unsuccesful. Therefore, we concluded that the checking if the windows update service
    is turned off, then there is no way for windows to update, and the additional test fixtures
    (that were failing in gcp and passing elsewhere) are not neccessary.
    [#151438181]
    
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

--- bosh-windows-acceptance-tests/assets/bwats-release/jobs/verify-autoupdates/templates/run.ps1
+++ bosh-windows-acceptance-tests/assets/bwats-release/jobs/verify-autoupdates/templates/run.ps1
@@ -7,47 +7,7 @@ if ((Get-Service wuauserv).Status -ne "Stopped") {
 
 $StartType = (Get-Service wuauserv).StartType
 if ($StartType -ne "Disabled") {
-    # On some IaaS's (AWS) updates are disabled, but the service is not running.
     Write-Host "Warning: wuauserv service StartType is not disabled: ${StartType}"
-}
-
-$AutoUpdateRegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update"
-
-$ExpectedAUOptions = 1
-$ExpectedEnableFeaturedSoftware = 0
-$ExpectedIncludeRecommendedUpdates = 0
-
-$AutoUpdateProperties = Get-ItemProperty -Path $AutoUpdateRegistryPath
-
-if ($AutoUpdateProperties.AUOptions -and
-    $AutoUpdateProperties.AUOptions -ne $ExpectedAUOptions) {
-    Write-Error ("Error: Expected AUOptions to be '{0}', got '{1}'" -f `
-        $ExpectedAUOptions, ($AutoUpdateProperties.AUOptions))
-    $ReturnCode = 1
-}
-
-# If the AUOptions key is not present (for example GCP) check CachedAUOptions.
-if ($AutoUpdateProperties.AUOptions -eq $null) {
-    if ($AutoUpdateProperties.CachedAUOptions -ne $null -and
-        $AutoUpdateProperties.CachedAUOptions -ne $ExpectedAUOptions) {
-        Write-Error ("Error: Expected CachedAUOptions to be '{0}', got '{1}'" -f `
-            $ExpectedAUOptions, ($AutoUpdateProperties.CachedAUOptions))
-        $ReturnCode = 1
-    }
-}
-
-if ($AutoUpdateProperties.EnableFeaturedSoftware -and
-    $AutoUpdateProperties.EnableFeaturedSoftware -ne $ExpectedEnableFeaturedSoftware) {
-    Write-Error ("Error: Expected EnableFeaturedSoftware to be '{0}' got '{1}'" -f `
-        $ExpectedEnableFeaturedSoftware, ($AutoUpdateProperties.EnableFeaturedSoftware))
-    $ReturnCode = 1
-}
-
-if ($AutoUpdateProperties.IncludeRecommendedUpdates -and
-    $AutoUpdateProperties.IncludeRecommendedUpdates -ne $ExpectedIncludeRecommendedUpdates) {
-    Write-Error ("Error: Expected IncludeRecommendedUpdates to be '{0}', got '{1}'" -f `
-        $ExpectedIncludeRecommendedUpdates, ($AutoUpdateProperties.IncludeRecommendedUpdates))
-    $ReturnCode = 1
 }
 
 Exit $ReturnCode
