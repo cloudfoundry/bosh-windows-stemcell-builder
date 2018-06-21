7badf14d3a7c0569378c5a8034e9f1b1649afd8d
Zander Mackie <amackie@pivotal.io>
2018-06-21 10:29:36 -0400

    Run cacls.exe via cmd.exe
    [#157010537](https://www.pivotaltracker.com/story/show/157010537)
    
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

--- bosh-psmodules/modules/BOSH.Utils/BOSH.Utils.psm1
+++ bosh-psmodules/modules/BOSH.Utils/BOSH.Utils.psm1
@@ -88,19 +88,19 @@ function Protect-Dir {
     }
 
     Write-Log "Protect-Dir: Remove BUILTIN\Users"
-    cacls.exe $path /T /E /R "BUILTIN\Users"
+    cmd.exe /c cacls.exe $path /T /E /R "BUILTIN\Users"
     if ($LASTEXITCODE -ne 0) {
         Throw "Error setting ACL for $path exited with $LASTEXITCODE"
     }
 
     Write-Log "Protect-Dir: Remove BUILTIN\IIS_IUSRS"
-    cacls.exe $path /T /E /R "BUILTIN\IIS_IUSRS"
+    cmd.exe /c cacls.exe $path /T /E /R "BUILTIN\IIS_IUSRS"
     if ($LASTEXITCODE -ne 0) {
         Throw "Error setting ACL for $path exited with $LASTEXITCODE"
     }
 
     Write-Log "Protect-Dir: Grant Administrator"
-    cacls.exe $path /T /E /P Administrators:F
+    cmd.exe /c cacls.exe $path /T /E /P Administrators:F
     if ($LASTEXITCODE -ne 0) {
         Throw "Error setting ACL for $path exited with $LASTEXITCODE"
     }
