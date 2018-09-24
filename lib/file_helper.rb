f7234f6577ab08b4a53bc9d20ae0cdf5e0679123
Malini Valliath <mvalliath@pivotal.io>
2018-09-24 15:46:47 -0400

    VHD parsing works for all supported os versions
    [#160270298](https://www.pivotaltracker.com/story/show/160270298)
    
    Signed-off-by: Simon Jones <sijones@pivotal.io>

--- lib/file_helper.rb
+++ lib/file_helper.rb
@@ -1,6 +1,11 @@
 class FileHelper
   def self.parse_vhd_version(vhd_filename)
     md = vhd_filename.match(/.+\-Containers\-(\d+).+$/)
+
+    if md.nil?
+      md = vhd_filename.match(/.+\.patched-(\d+-\d+)$/)
+    end
+
     if md[1]
       return md[1]
     else
--- spec/file_helper_spec.rb
+++ spec/file_helper_spec.rb
@@ -2,9 +2,14 @@ require 'file_helper'
 
 describe FileHelper do
   describe '.parse_vhd_version' do
-    it 'parses the version from input vhd' do
+    it 'parses the version from input windows 1709 vhd' do
       vhd_filename = 'some.file-Containers-170823-en-us'
       expect(FileHelper.parse_vhd_version(vhd_filename)).to eq('170823')
+    end
+
+    it 'parses the version from input windows 2012R2 vhd' do
+      vhd_filename = 'some.file.patched-170802-110000'
+      expect(FileHelper.parse_vhd_version(vhd_filename)).to eq('170802-110000')
     end
   end
 end
