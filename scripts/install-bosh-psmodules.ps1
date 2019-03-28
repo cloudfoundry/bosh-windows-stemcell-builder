dd42f7bf6a7ca3219abb9f98428278f62c99b820
Simon Jones <sijones@pivotal.io>
2019-03-28 14:07:31 -0400

    Make provisioners more resilient
    - the unzip in this script was flaky because the zip file does not
      always download before it is called.
    - we have also seen flakes on steps in provision because the BOSH.Utils
      module wasn't imported before a function in the module was called and
      powershell couldn't find the fucntion in time
    
    [#164964613](https://www.pivotaltracker.com/story/show/164964613)

--- scripts/install-bosh-psmodules.ps1
+++ scripts/install-bosh-psmodules.ps1
@@ -15,4 +15,7 @@ function Unzip {
 
 $path = "C:\Program Files\WindowsPowerShell\Modules"
 Remove-Item -Path (Join-Path $path "BOSH.*") -Force -Recurse
+
+while (!(Test-Path "C:\provision\bosh-psmodules.zip")) { Start-Sleep 5 }
 Unzip -ZipFile "C:\provision\bosh-psmodules.zip" -OutPath $path -Keep $false
+Import-Module BOSH.Utils
