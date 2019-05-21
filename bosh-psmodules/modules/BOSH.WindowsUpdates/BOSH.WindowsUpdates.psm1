60eb038eeedd08afbe07c5188c6f37ccc0f5a4e4
Gab Satchi <gsatchithanantham@pivotal.io>
2019-05-21 14:32:53 -0400

    Update registry key for zombie load
    [#166159868](https://www.pivotaltracker.com/story/show/166159868)
     Ensure that all required registry/policy changes are present for zombie CVE
    
    Co-authored-by: Garima Sharma <gasharma@pivotal.io>

--- bosh-psmodules/modules/BOSH.WindowsUpdates/BOSH.WindowsUpdates.psm1
+++ bosh-psmodules/modules/BOSH.WindowsUpdates/BOSH.WindowsUpdates.psm1
@@ -53,8 +53,8 @@ function Install-WindowsUpdates {
     REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\QualityCompat /f /v cadca5fe-87d3-4b96-b7fb-a231484277cc /t REG_DWORD /d 0
 
     # Set registry keys so that KB4056898 will be enabled
-    # Set registry keys to cover CVE-2018-3639, CVE-2017-5715 and CVE-2017-5754
-    reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverride /t REG_DWORD /d 8 /f
+    # Set registry keys to cover CVE-2018-11091 CVE-2018-12126 CVE-2018-12127 CVE-2018-12130 CVE-2017-5753 CVE-2017-5715 CVE-2017-5754 CVE-2018-3639 CVE-2018-3615 CVE-2018-3620 CVE-2018-3646
+    reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverride /t REG_DWORD /d 72 /f
     reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverrideMask /t REG_DWORD /d 3 /f
     reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Virtualization" /v MinVmVersionForCpuBasedMitigations /t REG_SZ /d "1.0" /f
 
