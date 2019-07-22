833c91679edfb7926503fc025e63990cca9056ac
Daniel Basner <dbasner@pivotal.io>
2019-07-22 11:58:25 -0400

    WIP mob commit. Added matcher for provisioners. Added ability to check provsioner ordering.
    Co-authored-by: Tal Usvyatsky <tusvyatsky@pivotal.io>

--- spec/packer/config/provisioners_spec.rb
+++ spec/packer/config/provisioners_spec.rb
@@ -1,3 +1,58 @@
+require 'rspec/expectations'
+
+RSpec::Matchers.define :include_provisioner do |expected_provisioner, after:nil|
+  match do |actual_provisioners|
+    provisioner_index = actual_provisioners.find_index do |provisioner|
+      expected_provisioner.matches? provisioner
+    end
+
+    #TODO consider turning this into a recursive function that checks a subset of the provisioners
+    # allow after to be an array of provisioners
+    if after == nil
+      return provisioner_index != nil
+    else
+      after_index = actual_provisioners.find_index do |provisioner|
+        after.matches? provisioner
+      end
+      return provisioner_index != nil && after_index != nil && provisioner_index > after_index
+    end
+  end
+end
+
+class TestProvisioner
+  attr_accessor :command, :source, :destination, :provisioner_type
+
+  def self.new_file_provisioner(source, destination)
+    provisioner = new
+    provisioner.provisioner_type = :file
+    provisioner.source = source
+    provisioner.destination = destination
+    provisioner
+  end
+
+  def self.new_powershell_provisioner(command)
+    provisioner = new
+    provisioner.provisioner_type = :powershell
+    provisioner.command = command
+    provisioner
+  end
+
+  def matches?(actual_provisioner)
+    case @provisioner_type
+      when :powershell
+        actual_provisioner['type'] == 'powershell' &&
+        actual_provisioner.has_key?('inline') &&
+        actual_provisioner['inline'].include?(@command)
+      when :file
+        actual_provisioner['type'] == 'file' &&
+        actual_provisioner['source'] == @source &&
+        actual_provisioner['destination'] == @destination
+      else
+        false
+    end
+  end
+end
+
 describe 'provisioners' do
   before(:context) do
     stemcell_deps_dir = Dir.mktmpdir('gcp')
@@ -18,35 +73,50 @@ describe 'provisioners' do
     ENV.delete('STEMCELL_DEPS_DIR')
   end
 
+  it 'does not have nonsense provisioner' do
+    nonsense_provisioner = TestProvisioner.new_powershell_provisioner('some-garbage')
+    expect(@provisioners).not_to include_provisioner(nonsense_provisioner), 'test matcher'
+  end
+
   it 'uploads bosh ps-modules' do
-    expect(@provisioners.detect do |x|
-      x['type'] == 'file' && x['source'] == 'build/bosh-psmodules.zip' && x['destination'] == 'C:\provision\bosh-psmodules.zip'
-    end).not_to be_nil
+    upload_bosh_ps_modules = TestProvisioner.new_file_provisioner('build/bosh-psmodules.zip', 'C:\provision\bosh-psmodules.zip')
+    expect(@provisioners).to include_provisioner(upload_bosh_ps_modules)
   end
 
   it 'uploads the install-bosh-psmodules script' do
-    expect(@provisioners.detect do |x|
-      x['type'] == 'file' && x['source'] == 'scripts/install-bosh-psmodules.ps1' && x['destination'] == 'C:\provision\install-bosh-psmodules.ps1'
-    end).not_to be_nil
+    upload_install_bosh_ps_modules = TestProvisioner.new_file_provisioner(
+        'scripts/install-bosh-psmodules.ps1',
+        'C:\provision\install-bosh-psmodules.ps1'
+    )
+    expect(@provisioners).to include_provisioner(upload_install_bosh_ps_modules)
   end
 
   it 'runs install bosh ps modules after uploading zip file and install script' do
-    prov_index = @provisioners.find_index do |x|
-      x['type'] == 'powershell' && x.has_key?('inline') && x['inline'].include?('C:\provision\install-bosh-psmodules.ps1')
-    end
+    #TODO use test provisioner matcher, while driving out order
+    install_modules_provisioner = TestProvisioner.new_powershell_provisioner('C:\provision\install-bosh-psmodules.ps1')
+    upload_modules = TestProvisioner.new_file_provisioner('build/bosh-psmodules.zip', 'C:\provision\bosh-psmodules.zip')
+    upload_install_modules = TestProvisioner.new_file_provisioner(
+        'scripts/install-bosh-psmodules.ps1',
+        'C:\provision\install-bosh-psmodules.ps1'
+    )
 
-    expect(prov_index).not_to be_nil
-
-    zip_index = @provisioners.find_index do |x|
-      x['type'] == 'file' && x['source'] == 'build/bosh-psmodules.zip' && x['destination'] == 'C:\provision\bosh-psmodules.zip'
-    end
-
-    install_script_index = @provisioners.find_index do |x|
-      x['type'] == 'file' && x['source'] == 'scripts/install-bosh-psmodules.ps1' && x['destination'] == 'C:\provision\install-bosh-psmodules.ps1'
-    end
-
-    expect(prov_index).to be > zip_index
-    expect(prov_index).to be > install_script_index
+    expect(@provisioners).to include_provisioner(install_modules_provisioner, after:upload_install_modules)
+    # prov_index = @provisioners.find_index do |x|
+    #   x['type'] == 'powershell' && x.has_key?('inline') && x['inline'].include?('C:\provision\install-bosh-psmodules.ps1')
+    # end
+    #
+    # expect(prov_index).not_to be_nil
+    #
+    # zip_index = @provisioners.find_index do |x|
+    #   x['type'] == 'file' && x['source'] == 'build/bosh-psmodules.zip' && x['destination'] == 'C:\provision\bosh-psmodules.zip'
+    # end
+    #
+    # install_script_index = @provisioners.find_index do |x|
+    #   x['type'] == 'file' && x['source'] == 'scripts/install-bosh-psmodules.ps1' && x['destination'] == 'C:\provision\install-bosh-psmodules.ps1'
+    # end
+    #
+    # expect(prov_index).to be > zip_index
+    # expect(prov_index).to be > install_script_index
   end
 
   it 'runs get-hotfix after hotfixes applied' do
@@ -70,6 +140,10 @@ describe 'provisioners' do
   end
 
   it 'Runs Unregister windows update after the post-RegisterWindowsUpdates windows-restart' do
+    register_windows_updates_task_provisioner = TestProvisioner.new_powershell_provisioner('Register-WindowsUpdatesTask')
+    expect(@provisioners).to include_provisioner(register_windows_updates_task_provisioner), 'test matcher'
+
+
     # noinspection RubyInterpreter
     register_updates_index = @provisioners.find_index do |p|
       p['type'] == 'powershell' && p.has_key?('inline') && p['inline'].include?('Register-WindowsUpdatesTask')
