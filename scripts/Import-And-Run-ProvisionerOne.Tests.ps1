3a470ff7acb1a9233e12528c0d0a54cd34c39ccc
Flora Gallina-Jones <fgallinajones@pivotal.io>
2019-11-18 11:35:03 -0500

    finish extract-archive'ing, work on set-proxy
    in Import-and-run
    
    Co-authored-by: Tal Usvyatsky <tusvyatsky@pivotal.io>
    Co-authored-by: Daniel Basner <dbasner@pivotal.io>
    Co-authored-by: George Gelashvili <ggelashvili+cf@pivotal.io>
    Co-authored-by: Sam Coward <scoward@pivotal.io>

--- scripts/Import-And-Run-ProvisionerOne.Tests.ps1
+++ scripts/Import-And-Run-ProvisionerOne.Tests.ps1
@@ -1,7 +1,28 @@
 Describe "Import-And-Run-ProvisionerOne.Tests.ps1" {
+    BeforeEach {
+        function Set-ProxySettings
+        {
+            param ([string]$HTTPProxy, [string]$HTTPSProxy)
+        }
+    }
     It "unzips bosh-psmodules" {
-        Mock Expand-Archive {}
+        Mock Expand-Archive { }
         .\Import-And-Run-ProvisionerOne.ps1
-       Assert-MockCalled Expand-Archive -Times 1 -Scope It
+        $outPath = "C:\Program Files\WindowsPowerShell\Modules"
+        Assert-MockCalled Expand-Archive -Times 1 -Scope It -ParameterFilter {
+            $Path -eq "C:\provision\bosh-psmodules.zip" -and $DestinationPath -eq $outPath
+        }
+    }
+
+    Context "when proxy settings are passed" {
+        It "sets proxy settings" {
+            Mock Set-ProxySettings { }
+            $proxySetFlags = "-HTTPProxy my.proxy"# -HTTPSProxy secure.proxy"
+            .\Import-And-Run-ProvisionerOne.ps1 $proxySetFlags
+            Assert-MockCalled Set-ProxySettings -Times 1 -Scope It -ParameterFilter {
+                Write-Host $HTTPProxy
+                $HTTPProxy -eq "my.proxy"
+            }
+        }
     }
 }
--- scripts/Import-And-Run-ProvisionerOne.ps1
+++ scripts/Import-And-Run-ProvisionerOne.ps1
@@ -1,9 +1,14 @@
+param([string]$proxySettings)
+
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-Write-Host "HELLOOO"
-# .\install-bosh-psmodules.ps1
+$outPath = "C:\Program Files\WindowsPowerShell\Modules"
+Expand-Archive "C:\provision\bosh-psmodules.zip" -DestinationPath $outPath
 
+#Import-Module -Name BOSH.Utils
+
+Set-ProxySettings $proxySettings
 
 # Move the content of install-bosh-psmodules into here
 # directly, rather than calling that script.
