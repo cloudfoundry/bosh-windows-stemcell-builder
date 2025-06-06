4d737bd7751a7b3ab4c423ca453492bf8c640830
aram price <aram.price@broadcom.com>
2025-06-05 17:41:40 -0700

    CI: tasks run against `stemcell-builder`, not CI
    CI scripts should not use relative paths to address "production" code
    since the `ci/` code is not subject to the pass constraints in
    concourse.

--- ci/tasks/test-units-bosh-psmodules/run.ps1
+++ ci/tasks/test-units-bosh-psmodules/run.ps1
@@ -15,9 +15,7 @@ $startupType = Get-Service "wuauserv" | Select-Object -ExpandProperty StartType 
 
 $result = 0
 
-$repoRoot = "$PSScriptRoot\..\..\..\"
-
-$testModules = Get-ChildItem "$repoRoot\$env:MODULES_DIR" -recurse | where {$_.name -match ".*.Tests.ps1"} | foreach {$_.DirectoryName}
+$testModules = Get-ChildItem "stemcell-builder\$env:MODULES_DIR" -recurse | where {$_.name -match ".*.Tests.ps1"} | foreach {$_.DirectoryName}
 foreach ($module in $testModules) {
     Write-Host "Testing: $module"
     Push-Location "$module"
