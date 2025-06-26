2c2b9bca84ac93bc0320c06b0ef9d5ea5d1379fb
Rajath Agasthya <rajath.agasthya@broadcom.com>
2025-06-25 22:37:39 -0500

    Change windows version constant in acceptance tests

--- acceptance_test/main_test.go
+++ acceptance_test/main_test.go
@@ -27,7 +27,7 @@ const LgpoUrl = "https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FF
 const lgpoFile = "LGPO.exe"
 const redeployRetries = 10
 
-const windowsVersion = "windows-2019"
+const windowsVersion = "windows-2025"
 
 var (
 	boshCommand               *BoshCommand
