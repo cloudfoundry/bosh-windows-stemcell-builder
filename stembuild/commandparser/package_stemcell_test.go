3001665ce1180ec1cfbdbe8838bd3d46f71a5349
Rajath Agasthya <rajath.agasthya@broadcom.com>
2025-06-26 08:18:34 -0500

    Change version constants in stembuild tests to 2025

--- stembuild/commandparser/package_stemcell_test.go
+++ stembuild/commandparser/package_stemcell_test.go
@@ -48,8 +48,8 @@ var _ = Describe("package_stemcell", func() {
 
 		Describe("Execute", func() {
 			BeforeEach(func() {
-				oSAndVersionGetter.GetVersionReturns("2019.2")
-				oSAndVersionGetter.GetOsReturns("2019")
+				oSAndVersionGetter.GetVersionReturns("2025.2")
+				oSAndVersionGetter.GetOsReturns("2025")
 			})
 
 			It("packager is instantiated with expected vmdk source config", func() {
@@ -116,8 +116,8 @@ var _ = Describe("package_stemcell", func() {
 				Expect(packagerFactory.NewPackagerCallCount()).To(Equal(1))
 				_, actualOutputConfig, _ := packagerFactory.NewPackagerArgsForCall(0)
 				Expect(actualOutputConfig.OutputDir).To(Equal("some_output_dir"))
-				Expect(actualOutputConfig.StemcellVersion).To(Equal("2019.2"))
-				Expect(actualOutputConfig.Os).To(Equal("2019"))
+				Expect(actualOutputConfig.StemcellVersion).To(Equal("2025.2"))
+				Expect(actualOutputConfig.Os).To(Equal("2025"))
 			})
 
 			It("creates packager with correct stemcell patch version number when argument provided", func() {
