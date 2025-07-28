b36c0f7a2d96f504d3f7daedd7d01063f3479422
aram price <aram.price@broadcom.com>
2025-07-28 14:29:01 -0700

    Install and use Microsoft provide OpenSSH
    This replaces the OpenSSH which had been installed using
    OpenSSH-Win64.zip. The configuration file structure and location have
    changed slightly.
    
    Remove some trailing `()` on function definitions

--- modules/BOSH.SSH/BOSH.SSH.psm1
+++ modules/BOSH.SSH/BOSH.SSH.psm1
@@ -1,95 +1,34 @@
 ﻿function Install-SSHD
 {
-    param (
-        [string]$SSHZipFile = $( Throw "Provide an SSHD zipfile" )
-    )
+    if (Get-OSVersion == "windows2019") {
+        # Microsoft privided OpenSSH must be installed on Windows 2019
+        # => https://learn.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse?tabs=powershell&pivots=windows-server-2019
+        $sshPackages = @("OpenSSH.Client", "OpenSSH.Server")
+        foreach ($sshPackage in $sshPackages)
+        {
+            Get-WindowsCapability -Online -Name "$sshPackage*" `
+                | Where-Object { $_.State -eq "NotPresent" } `
+                | Add-WindowsCapability -Online
+        }
+    }
 
-    New-Item "$env:PROGRAMFILES\SSHTemp" -Type Directory -Force
-    Open-Zip -ZipFile $SSHZipFile -OutPath "$env:PROGRAMFILES\SSHTemp"
-
-    $ConfigPath = "$env:PROGRAMFILES\SSHTemp\OpenSSH-Win64\sshd_config_default"
-    $ModifiedConfigContents = Edit-DefaultOpenSSHConfig -ConfigPath $ConfigPath
-    Remove-Item -Force $ConfigPath
-    Out-File -FilePath $ConfigPath -InputObject $ModifiedConfigContents -Encoding UTF8
-
-    Move-Item -Force "$env:PROGRAMFILES\SSHTemp\OpenSSH-Win64" "$env:PROGRAMFILES\OpenSSH"
-    Remove-Item -Force "$env:PROGRAMFILES\SSHTemp"
-
-    # Remove users from 'OpenSSH' before installing.  The install process
-    # will add back permissions for the NT AUTHORITY\Authenticated Users for some files
-    Protect-Dir -path "$env:PROGRAMFILES\OpenSSH"
-
-    Push-Location "$env:PROGRAMFILES\OpenSSH"
-    powershell -ExecutionPolicy Bypass -File install-sshd.ps1
-    Pop-Location
-
-    #    # Grant NT AUTHORITY\Authenticated Users access to .EXEs and the .DLL in OpenSSH
-    $FileNames = @(
-        "libcrypto.dll",
-        "scp.exe",
-        "sftp-server.exe",
-        "sftp.exe",
-        "ssh-add.exe",
-        "ssh-agent.exe",
-        "ssh-keygen.exe",
-        "ssh-keyscan.exe",
-        "ssh-shellhost.exe",
-        "ssh.exe",
-        "sshd.exe"
-    )
-    Invoke-CACL -FileNames $FileNames
+    Edit-DefaultOpenSSHConfig
 
     Set-Service -Name sshd -StartupType Disabled
-    # ssh-agent is not the same as ssh-agent in *nix openssh
     Set-Service -Name ssh-agent -StartupType Disabled
 }
 
 function Enable-SSHD
 {
-    if ($null -eq (Get-NetFirewallRule | Where-Object { $_.DisplayName -ieq 'SSH' }))
-    {
-        "Creating firewall rule for SSH"
-        New-NetFirewallRule -Protocol TCP -LocalPort 22 -Direction Inbound -Action Allow -DisplayName SSH
+    # Remove existing OpenSSH firewall rule and recreate with '-Profile Any' option
+    if (Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue) {
+        "Removing firewall rule: 'OpenSSH-Server-In-TCP'"
+        Remove-NetFirewallRule -Name "OpenSSH-Server-In-TCP"
     }
-    else
-    {
-        "Firewall rule for SSH already exists"
-    }
-
-    $InfFilePath = "$env:WINDIR\Temp\enable-ssh.inf"
-
-    $InfFileContents = @'
-[Unicode]
-Unicode=yes
-[Version]
-signature=$CHICAGO$
-Revision=1
-[Registry Values]
-[System Access]
-[Privilege Rights]
-SeDenyNetworkLogonRight=*S-1-5-32-546
-SeAssignPrimaryTokenPrivilege=*S-1-5-19,*S-1-5-20,*S-1-5-80-3847866527-469524349-687026318-516638107-1125189541
-'@
-    $LGPOPath = "$env:WINDIR\LGPO.exe"
-    if (Test-Path $LGPOPath)
-    {
-        Out-File -FilePath $InfFilePath -Encoding unicode -InputObject $InfFileContents -Force
-        Try
-        {
-            Invoke-LGPO -LGPOPath $LGPOPath -InfFilePath $InfFilePath
-        }
-        Catch
-        {
-            throw "LGPO.exe failed with: $_.Exception.Message"
-        }
-    }
-    else
-    {
-        "Did not find $LGPOPath. Assuming existing security policies are sufficient to support ssh."
-    }
+    Write-Output "Creating firewall rule 'OpenSSH-Server-In-TCP'"
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -Profile Any -LocalPort 22
 
     Set-Service -Name sshd -StartupType Automatic
-    # ssh-agent is not the same as ssh-agent in *nix openssh
     Set-Service -Name ssh-agent -StartupType Automatic
 
     Remove-SSHKeys
@@ -97,48 +36,36 @@ SeAssignPrimaryTokenPrivilege=*S-1-5-19,*S-1-5-20,*S-1-5-80-3847866527-469524349
 
 function Remove-SSHKeys
 {
-    $SSHDir = "C:\Program Files\OpenSSH"
-
-    Push-Location $SSHDir
-    New-Item -ItemType Directory -Path "$env:ProgramData\ssh" -ErrorAction Ignore
-
     "Removing any existing host keys"
     Remove-Item -Path "$env:ProgramData\ssh\ssh_host_*" -ErrorAction Ignore
-    Pop-Location
-}
-
-function Invoke-CACL
-{
-    param (
-        [string[]] $FileNames = $( Throw "Files not provided" )
-    )
-
-    foreach ($name in $FileNames)
-    {
-        $path = Join-Path "$env:PROGRAMFILES\OpenSSH" $name
-        cacls.exe $Path /E /P "NT AUTHORITY\Authenticated Users:R"
-    }
-}
-
-function Invoke-LGPO
-{
-    param (
-        [string]$LGPOPath = $( Throw "Provide LGPO path" ),
-        [string]$InfFilePath = $( Throw "Provide Inf file path" )
-    )
-    & $LGPOPath /s $InfFilePath
 }
 
 function Edit-DefaultOpenSSHConfig
 {
     param (
-        [string]$ConfigPath = $( Throw "Provide openssh default config path" )
+        [string]$ConfigPath = "$env:windir\System32\OpenSSH\sshd_config_default",
+        [string]$GeneratedConfigPath = "$env:ProgramData\ssh\sshd_config"
     )
 
-    $ModifiedConfig = Get-Content $ConfigPath `
-    | ForEach-Object{ $_ -replace ".*Match Group administrators.*", "#$&" } `
-    | ForEach-Object{ $_ -replace ".*AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys.*", "#$&" } `
-    | ForEach-Object{ $_ -replace "#RekeyLimit default none", "$&`r`n# Disable cipher to mitigate CVE-2023-48795`r`nCiphers -chacha20-poly1305@openssh.com`r`n" }
+    Copy-Item -Path $ConfigPath -Destination "$ConfigPath.bak"
 
-    return $ModifiedConfig
+    $OriginalConfig = Get-Content $ConfigPath
+    Write-Output "Original SSH config at $ConfigPath :"
+    Write-Output $OriginalConfig
+
+    $ModifiedConfig = $OriginalConfig `
+        | ForEach-Object{ $_ -replace ".*Match Group administrators.*", "#$&" } `
+        | ForEach-Object{ $_ -replace ".*AllowGroups administrators.*", "#$&" } `
+        | ForEach-Object{ $_ -replace ".*AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys.*", "#$&" } `
+        | ForEach-Object{ $_ -replace "#RekeyLimit default none", "$&`r`n# Disable cipher to mitigate CVE-2023-48795`r`nCiphers -chacha20-poly1305@openssh.com`r`n" }
+
+    Write-Output "Modified SSH config at $ConfigPath :"
+    Write-Output $ModifiedConfig
+
+    Remove-Item -Force $ConfigPath
+    Out-File -FilePath $ConfigPath -InputObject $ModifiedConfig -Encoding UTF8
+
+    # We need to make sure that the generated config is cleared, so our above changes are applied when the config
+    # is next generated. If this isnt done, then we may have a config from the prior template.
+    Remove-Item -Path $GeneratedConfigPath -ErrorAction Ignore
 }
