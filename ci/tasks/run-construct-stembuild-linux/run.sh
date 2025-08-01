0d0389aba9f04e1c381858edebf92d4a6ac3363a
Rajath Agasthya <rajath.agasthya@broadcom.com>
2025-08-01 16:46:44 -0500

    Use govc to fetch base VM IP during stembuild
    Removes hardcoding IP for the base VMs in the pipeline. This should also
    theoretically remove static IP requirement for the base VM.

--- ci/tasks/run-construct-stembuild-linux/run.sh
+++ ci/tasks/run-construct-stembuild-linux/run.sh
@@ -16,10 +16,21 @@ mv stembuild-untested-linux/stembuild .
 
 mv lgpo-binary/LGPO*.zip LGPO.zip
 
+VM_NAME="${VCENTER_VM_FOLDER}/${STEMBUILD_BASE_VM_NAME}"
+
+echo "Fetching IP for VM: $VM_NAME..."
+
+VM_IP=$(govc vm.info -json "$VM_NAME" | jq -r '.virtualMachines[0].guest.ipAddress')
+
+if [ -z "$VM_IP" ]; then
+  echo "Error: could not retrieve IP address for VM '$VM_NAME'."
+  exit 1
+fi
+
 chmod 500 stembuild
 ./stembuild construct \
   -vcenter-url ${VCENTER_BASE_URL} -vcenter-username ${VCENTER_USERNAME} -vcenter-password ${VCENTER_PASSWORD} \
   -vcenter-ca-certs ca.crt \
-  -vm-inventory-path ${VCENTER_VM_FOLDER}/${STEMBUILD_BASE_VM_NAME} \
-  -vm-ip ${STEMBUILD_BASE_VM_IP} -vm-username ${STEMBUILD_BASE_VM_USERNAME} -vm-password ${STEMBUILD_BASE_VM_PASSWORD} \
+  -vm-inventory-path ${VM_NAME} \
+  -vm-ip ${VM_IP} -vm-username ${STEMBUILD_BASE_VM_USERNAME} -vm-password ${STEMBUILD_BASE_VM_PASSWORD} \
   -setup-arg FailOnInstallWUCerts
